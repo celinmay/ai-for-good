d916ca1369b32bfa4071eafe3ee1c39e9773242b
C21AI <brosboel@hotmail.dk>
2020-11-07 12:17:53 +0100

    Feeding the ANN
    debugging

--- Training the AI/artificial_neural_network.py
+++ Training the AI/artificial_neural_network.py
@@ -9,39 +9,30 @@ tf.__version__
 # Part 1 - Data Preprocessing
 
 # Importing the dataset
-dataset = pd.read_csv('symptom_frequency.csv') # Enter dataset (Not done)
-X = dataset.iloc[:,:20].values # Explanatory variables (Not done)
-y = dataset.iloc[:,20:].values # Response variable (Not done)
-#print(X)
-#print(y)
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
 
-# ENCODING CATEGORICAL DATA
-# Label Encoding to binary column
+# Encoding categorical data
+# Label Encoding the "Gender" column
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-i = 0
-for i in range(20):
-    X[:,i] = le.fit_transform(X[:,i])
-    i += 1
+X[:, 2] = le.fit_transform(X[:, 2])
+print(X)
+# One Hot Encoding the "Geography" column
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
 print(X)
 
-le_2 = LabelEncoder()
-
-y[:,0] = le_2.fit_transform(y[:,0])
-
-# One Hot Encoding the categorical column with more than two categories
-#from sklearn.compose import ColumnTransformer
-#from sklearn.preprocessing import OneHotEncoder
-#ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [])], remainder='passthrough') # Enter column of interest (Not done)
-#X = np.array(ct.fit_transform(X))
-#print(X)
-# END OF CATEGORICAL DATA
-
 # Feature Scaling
-#from sklearn.preprocessing import StandardScaler
-#sc = StandardScaler()
-#X = sc.fit_transform(X)
-#print(X)
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+print(X)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
