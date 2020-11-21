7dc544ad43e969a1d15c21a61bb15f1898253d72
celinmay <62461350+celinmay@users.noreply.github.com>
2020-11-21 10:39:42 +0100

    adjusted flask template

--- AI for good/Training_the_AI/test.py
+++ AI for good/Training_the_AI/test.py
@@ -1,11 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 #Simple GET request to test app 
 
+import csv 
+
 @app.route('/', methods=['GET'])
 def home():
     return "Hello World"
+
+
     
 #Execute server
 if __name__ == '__main__':
